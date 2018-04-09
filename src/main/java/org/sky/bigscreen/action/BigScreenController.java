76c9dcf3dee9d275d77219872519c5c94048c8ff
weifx <weifx@192.168.0.104>
2018-04-09 12:10:39 +0800

    a

--- src/main/java/org/sky/bigscreen/action/BigScreenController.java
+++ src/main/java/org/sky/bigscreen/action/BigScreenController.java
@@ -29,7 +29,6 @@ public class BigScreenController {
 		mv.setViewName("jsp/bigscreen/demo");
 		return mv;
 	}
-	 
 	@RequestMapping(value = "/bigscreen/demo2", method = { RequestMethod.GET })
 	public ModelAndView demo2(Map<String, Object> map,
 			HttpServletRequest request, HttpServletResponse response) {
