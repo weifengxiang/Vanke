5801434cace461eb93819ba7fb91cfb8bb8f7203
weifengxiang <442695429@qq.com>
2018-05-30 12:42:38 +0800

    小程序后台

--- src/main/java/org/sky/miniapp/service/MiniAppService.java
+++ src/main/java/org/sky/miniapp/service/MiniAppService.java
@@ -1,28 +1,49 @@
 package org.sky.miniapp.service;
 
 import java.sql.Timestamp;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.sky.base.client.BaseAccountDetailMapper;
+import org.sky.base.client.BaseAccountMapper;
+import org.sky.base.client.BaseBankCardMapper;
 import org.sky.base.client.BaseChannelImgMapper;
 import org.sky.base.client.BaseChannelMapper;
 import org.sky.base.client.BaseCustomerMapper;
+import org.sky.base.client.BaseMsgMapper;
+import org.sky.base.client.BaseParttimeJobEnrollMapper;
 import org.sky.base.client.BasePhoneVerificationMapper;
+import org.sky.base.client.BaseWithdrawCashMapper;
+import org.sky.base.model.BaseAccount;
+import org.sky.base.model.BaseAccountDetail;
+import org.sky.base.model.BaseAccountDetailExample;
+import org.sky.base.model.BaseAccountExample;
+import org.sky.base.model.BaseBankCard;
+import org.sky.base.model.BaseBankCardExample;
 import org.sky.base.model.BaseChannel;
 import org.sky.base.model.BaseChannelExample;
 import org.sky.base.model.BaseChannelImgExample;
 import org.sky.base.model.BaseChannelImgWithBLOBs;
 import org.sky.base.model.BaseCustomer;
 import org.sky.base.model.BaseCustomerExample;
+import org.sky.base.model.BaseMsg;
+import org.sky.base.model.BaseMsgExample;
+import org.sky.base.model.BaseParttimeJobEnroll;
+import org.sky.base.model.BaseParttimeJobEnrollExample;
 import org.sky.base.model.BasePhoneVerification;
 import org.sky.base.model.BasePhoneVerificationExample;
+import org.sky.base.model.BaseWithdrawCash;
+import org.sky.base.model.BaseWithdrawCashExample;
 import org.sky.base.service.BaseCodeService;
+import org.sky.land.client.LandParttimeJobMapper;
+import org.sky.land.model.LandParttimeJob;
+import org.sky.land.model.LandParttimeJobExample;
 import org.sky.miniapp.utils.MiniAppUtils;
 import org.sky.sys.client.SysCommonMapper;
 import org.sky.sys.client.SysDictItemMapper;
 import org.sky.sys.exception.ServiceException;
-import org.sky.sys.model.SysDictItemExample;
 import org.sky.sys.utils.AliyunSmsUtils;
 import org.sky.sys.utils.ApplicationCached;
 import org.sky.sys.utils.CommonUtils;
@@ -48,9 +69,21 @@ public class MiniAppService {
 	@Autowired
 	private SysCommonMapper comMapper;
 	@Autowired
-	private SysDictItemMapper itemMapper;
-	@Autowired
 	private BaseCodeService bcService;
+	@Autowired
+	private BaseBankCardMapper bbcMapper;
+	@Autowired
+	private BaseAccountMapper bacMapper;
+	@Autowired
+	private BaseWithdrawCashMapper bwcMapper;
+	@Autowired
+	private BaseAccountDetailMapper badMapper;
+	@Autowired
+	private LandParttimeJobMapper lpjMapper;
+	@Autowired
+	private BaseParttimeJobEnrollMapper bptjMapper;
+	@Autowired
+	private BaseMsgMapper bMsgMapper;
 	/**
 	 * 根据电话号码获取渠道信息
 	 * @param tel
@@ -367,5 +400,163 @@ public class MiniAppService {
 		}else {
 			return null;
 		}
+	}
+	/**
+	 * 绑定银行卡
+	 * @param channelCode
+	 * @param bbc
+	 * @throws ServiceException
+	 */
+	public void saveAddBaseBankCard(String channelCode,BaseBankCard bbc) throws ServiceException{
+		BaseBankCardExample bbce = new BaseBankCardExample();
+		bbce.createCriteria().andCardCodeEqualTo(bbc.getCardCode()).andBankCodeEqualTo(bbc.getBankCode());
+		long count = bbcMapper.countByExample(bbce);
+		if(count>0) {
+			logger.error("渠道编号"+channelCode+"银行编号"+bbc.getBankCode()+"卡号"+bbc.getCardCode()+"银行卡已经绑定");
+			throw new ServiceException("银行卡已经绑定");
+		}
+		Timestamp ts = CommonUtils.getCurrentDbTimestamp();
+		bbc.setId(CommonUtils.getUUID(32));
+		bbc.setBindingDate(ts);
+		bbc.setChaCode(channelCode);
+		bbc.setCreater(channelCode);
+		bbc.setCreateTime(ts);
+		bbc.setUpdater(channelCode);
+		bbc.setUpdateTime(ts);
+		bbcMapper.insertSelective(bbc);
+	}
+	/**
+	 * 查询绑定的银行卡
+	 * @param channelCode
+	 * @return
+	 */
+	public List<BaseBankCard> selectBaseBankCard(String channelCode){
+		BaseBankCardExample bbce = new BaseBankCardExample();
+		bbce.createCriteria().andChaCodeEqualTo(channelCode);
+		List<BaseBankCard> list = bbcMapper.selectByExample(bbce);
+		return list;
+	}
+	/**
+	 * 账户信息查询
+	 * @param channelCode
+	 * @return
+	 */
+	public BaseAccount getBaseAccount(String channelCode) throws ServiceException{
+		BaseAccountExample bae = new BaseAccountExample();
+		bae.createCriteria().andChaCodeEqualTo(channelCode);
+		List<BaseAccount> list = bacMapper.selectByExample(bae);
+		if(null==list || list.isEmpty()) {
+			throw new ServiceException("未查询到任何账户");
+		}
+		return list.get(0);
+	}
+	/**
+	 * 提现申请
+	 * @param channelCode
+	 * @param bwc
+	 */
+	public void saveAddBaseWithDrawCash(String channelCode,BaseWithdrawCash bwc) {
+		Timestamp ts = CommonUtils.getCurrentDbTimestamp();
+		bwc.setId(CommonUtils.getUUID(32));
+		bwc.setChaCode(channelCode);
+		bwc.setCreater(channelCode);
+		bwc.setCreateTime(ts);
+		bwc.setReqDate(ts);
+		bwc.setState("0");
+		bwc.setUpdater(channelCode);
+		bwc.setUpdateTime(ts);
+		bwcMapper.insertSelective(bwc);
+	}
+	/**
+	 * 提现进度查询
+	 * @param channelCode
+	 * @return
+	 */
+	public List<BaseWithdrawCash> selectBaseWithDrawCashProcess(String channelCode) {
+		BaseWithdrawCashExample bwce = new BaseWithdrawCashExample();
+		bwce.createCriteria().andChaCodeEqualTo(channelCode);
+		return bwcMapper.selectByExample(bwce);
+	}
+	/**
+	 * 交易明细查询
+	 * @param channelCode
+	 * @return
+	 */
+	public List<BaseAccountDetail> selectBaseAccountDetail(String channelCode) {
+		BaseAccountDetailExample bade = new BaseAccountDetailExample();
+		bade.createCriteria();
+		Map filter = new HashMap();
+		filter.put("exists(select 1 from base_account ba where ba.code = account_code and lower(ba.cha_code)='"+channelCode.toLowerCase()+"') and 1@=","1");
+		bade.integratedQuery(filter);
+		return badMapper.selectByExample(bade);
+	}
+	/**
+	 * 兼职任务查询
+	 * @return
+	 */
+	public List<LandParttimeJob> selectLandParttimeJob(){
+		LandParttimeJobExample lpje = new LandParttimeJobExample();
+		return lpjMapper.selectByExample(lpje);
+	}
+	/**
+	 * 兼职任务明细
+	 * @param code
+	 * @return
+	 */
+	public LandParttimeJob getLandParttimeJobDetail(String code) throws ServiceException{
+		LandParttimeJobExample lpje = new LandParttimeJobExample();
+		lpje.createCriteria().andCodeEqualTo(code);
+		List<LandParttimeJob> list = lpjMapper.selectByExample(lpje);
+		if(null==list || list.isEmpty()) {
+			throw new ServiceException("未查询到任何账户");
+		}
+		return list.get(0);
+	}
+	/**
+	 * 兼职报名
+	 * @param channelCode
+	 * @param bpe
+	 */
+	public void saveAddBaseParttimeJobEnroll(String channelCode,BaseParttimeJobEnroll bpe) throws ServiceException{
+		BaseParttimeJobEnrollExample bpje = new BaseParttimeJobEnrollExample();
+		bpje.createCriteria().andChaCodeEqualTo(channelCode).andJobCodeEqualTo(bpe.getJobCode());
+		long count = bptjMapper.countByExample(bpje);
+		if(count>0) {
+			throw new ServiceException("已经报名，不能重复报名");
+		}
+		Timestamp ts = CommonUtils.getCurrentDbTimestamp();
+		bpe.setChaCode(channelCode);
+		bpe.setCode(bcService.getNextBizCode("ENR"));
+		bpe.setCreater(channelCode);
+		bpe.setCreateTime(ts);
+		bpe.setEnrollDate(ts);
+		bpe.setId(CommonUtils.getUUID(32));
+		bpe.setState("0");
+		bpe.setUpdater(channelCode);
+		bpe.setUpdateTime(ts);
+		bptjMapper.insertSelective(bpe);
+	}
+	/**
+	 * 查询我的兼职
+	 * @param channelCode
+	 * @return
+	 */
+	public List<BaseParttimeJobEnroll> selectMyLandParttimeJob(String channelCode) {
+		BaseParttimeJobEnrollExample bpje = new BaseParttimeJobEnrollExample();
+		bpje.createCriteria();
+		Map filter = new HashMap();
+		filter.put("exists(select 1 from base_parttime_job_enroll b where b.job_code = land_parttime_job.code and lower(b.cha_code)='"+channelCode.toLowerCase()+"') and 1@=","1");
+		bpje.integratedQuery(filter);
+		return bptjMapper.selectByExample(bpje);
+	}
+	/**
+	 * 查询我的消息
+	 * @param channelCode
+	 * @return
+	 */
+	public List<BaseMsg> selectMyBaseMsg(String channelCode) {
+		BaseMsgExample bme = new BaseMsgExample();
+		bme.createCriteria().andReceiverEqualTo(channelCode);
+		return bMsgMapper.selectByExample(bme);
 	}
 }
